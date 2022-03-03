3e5f843a89fc7cb200ec3476455e9ffde755e051
Katsuragawa <andre.katsuragawa@gmail.com>
2022-03-03 08:55:33 -0300

    Rename variables

--- server.rb
+++ server.rb
@@ -1,13 +1,16 @@
 require "httparty"
 
-SLANG_KEY = ENV["SLANG_CHALLENGE_API_KEY"]
+SLANG_API_KEY = ENV["SLANG_CHALLENGE_API_KEY"]
 
-base_url = "https://api.slangapp.com/challenges/v1/activities"
+slang_api_url = "https://api.slangapp.com/challenges/v1/activities"
 
 begin
   response = HTTParty.get(
-    base_url,
-    headers: { "Content-Type": "application/json", Authorization: "Basic #{SLANG_KEY}" }
+    slang_api_url,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic #{SLANG_API_KEY}"
+    }
   )
 
   puts response.code == 200 ? response.body : response.message
