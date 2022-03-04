04d6b60eded1b4054d693fe76988c44db0d3ad3b
Katsuragawa <andre.katsuragawa@gmail.com>
2022-03-04 17:49:08 -0300

    Convert timestamp and parse by user

--- services/activities_manager.rb
+++ services/activities_manager.rb
@@ -5,6 +5,19 @@ class ActivitiesManager
     @activities = activities
   end
 
+  def parse_activities_by_user
+    convert_timestamp
+    sort_activities_by_answered_at
+    activities.group_by { |activity| activity["user_id"] }
+  end
+
+  def convert_timestamp
+    activities.each do |activity|
+      activity["first_seen_at"] = Time.parse(activity["first_seen_at"]).iso8601
+      activity["answered_at"] = Time.parse(activity["answered_at"]).iso8601
+    end
+  end
+
   def sort_activities_by_answered_at
     activities.sort! { |a, b| a["first_seen_at"] <=> b["first_seen_at"] }
   end
