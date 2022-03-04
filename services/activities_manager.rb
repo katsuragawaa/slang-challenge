5d864b598f24c3424c5cd5604493814aaec29522
Katsuragawa <andre.katsuragawa@gmail.com>
2022-03-04 17:45:45 -0300

    Sort activities by first_seen_at

--- services/activities_manager.rb
+++ services/activities_manager.rb
@@ -4,4 +4,8 @@ class ActivitiesManager
   def initialize(activities)
     @activities = activities
   end
+
+  def sort_activities_by_answered_at
+    activities.sort! { |a, b| a["first_seen_at"] <=> b["first_seen_at"] }
+  end
 end
