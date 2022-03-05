12ea873dfbba47bc4e8120b3dacb949a376e7c6d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-03-04 21:41:12 -0300

    Send users sessions to api

--- server.rb
+++ server.rb
@@ -5,23 +5,27 @@ require "./services/sort_activities_service"
 SLANG_API_KEY = ENV["SLANG_CHALLENGE_API_KEY"]
 
 slang_api_url = "https://api.slangapp.com/challenges/v1/activities"
+headers = { "Content-Type": "application/json", Authorization: "Basic #{SLANG_API_KEY}" }
 
 begin
-  response = HTTParty.get(
+  get_response = HTTParty.get(
     slang_api_url,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Basic #{SLANG_API_KEY}"
-    }
+    headers: headers
   )
 
-  puts response.message
+  puts "GET activities: #{get_response.message}"
 
-  result = JSON.parse(response.body, symbolize_names: true)
+  result = JSON.parse(get_response.body, symbolize_names: true)
   sorted_activities = SortActivitiesService.new(result[:activities]).sort
   users_sessions = UsersSessionsService.new(sorted_activities).create
 
-  puts users_sessions.to_json
+  post_response = HTTParty.post(
+    "#{slang_api_url}/sessions",
+    headers: headers,
+    body: { user_sessions: users_sessions }.to_json
+  )
+
+  puts "POST sessions: #{post_response.message}"
 rescue HTTParty::Error, SocketError => e
   puts e
 end
